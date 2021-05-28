5dddeee32a095d341e9d645ef7adc48bfc1c70b6
Vigneshmof10 <>
2021-05-28 07:45:27 +0000

    Initial commit

---
+++ program1.c
@@ -0,0 +1,11 @@
+// to find the sum of two numbers
+#include<stdio.h>
+int main(void)
+{
+  int a,b,c;
+  printf("\nEnter number:");
+  scanf("%d" "%d",&a,&b);
+
+  c=a+b;
+  printf("\n%d + %d = %d",a,b,c);
+}
